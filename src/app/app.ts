191136544398b717084613707d261e75a5ff98f7
SantamRC <santamdev404@gmail.com>
2025-06-01 21:44:15 +0530

    Added Navbar and Table Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Tasklist } from './components/tasklist/tasklist';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Tasklist],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
