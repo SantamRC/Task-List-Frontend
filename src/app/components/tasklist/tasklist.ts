69bdb7ea0751baf868696019c05c097639458816
SantamRC <santamdev404@gmail.com>
2025-06-03 00:49:19 +0530

    Added Delete Function

--- src/app/components/tasklist/tasklist.ts
+++ src/app/components/tasklist/tasklist.ts
@@ -3,6 +3,7 @@ import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { TaskModel } from '../../models/task.model';
 import { TasklistService } from '../../service/tasklist';
 import { TaskFormComponent } from '../form/form'
@@ -18,7 +19,7 @@ export class Tasklist {
 
   dataSource = new MatTableDataSource<TaskModel>();
 
-  constructor(private tasklistService: TasklistService,private dialog: MatDialog) { }
+  constructor(private tasklistService: TasklistService,private dialog: MatDialog,private snackBar: MatSnackBar) { }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -43,6 +44,26 @@ export class Tasklist {
   onDelete(task: TaskModel) {
     console.log('Delete clicked for:', task);
     // confirm & call API to delete
+    const snackBarRef = this.snackBar.open('Are you sure you want to delete this task?', 'Yes', {
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['confirm-snackbar'] // optional for styling
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      // User clicked "Yes"
+      this.tasklistService.deleteTask(task.id).subscribe({
+        next: () => {
+          this.snackBar.open('Task deleted successfully!', '', { duration: 2000 });
+          window.location.reload()
+        },
+        error: (err) => {
+          console.error('Delete failed:', err);
+          this.snackBar.open('Failed to delete task.', '', { duration: 2000 });
+        }
+      });
+    });
   }
 
 }
