9c5a0fe9f9e1b0826d76de1dac1e2bc79ef7ae09
SantamRC <santamdev404@gmail.com>
2025-06-04 02:30:16 +0530

    Added a small change due to deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
-import { Tasklist } from './components/tasklist/tasklist';
 import { FilteredTaskListComponent } from './components/filtered-tasklist/filtered-tasklist';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Tasklist, FilteredTaskListComponent],
+  imports: [RouterOutlet, Navbar, FilteredTaskListComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
